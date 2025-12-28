976031d2a378bf57fe3a9e65876da16c8834729a
amrita09091999-prog <amrita09091999@gmail.com>
2025-12-28 15:18:53 +0530

    latest commit

---
+++ evaluation.py
@@ -0,0 +1,9 @@
+from rag import RAG
+import json
+import os 
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+
+query and context 
+answer and context 
+query and answer 
